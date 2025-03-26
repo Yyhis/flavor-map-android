4c4f75025896ce5ccff5b153e43d1cf1edb1a292
hyss <oouoo717@gmail.com>
2025-03-26 17:27:05 +0900

    chore: 라이브러리 버전을 카탈로그로 관리.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,28 +82,26 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation ("com.google.android.material:material:1.7.0")
+    implementation (libs.material)
 
-    implementation ("androidx.compose.runtime:runtime-livedata:1.7.8")
+    implementation (libs.androidx.runtime.livedata)
 
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation (libs.androidx.lifecycle.viewmodel.compose)
 
-    implementation ("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation (libs.androidx.security.crypto)
 
-    implementation ("com.jakewharton.timber:timber:5.0.1")
+    implementation (libs.timber)
 
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation (libs.circleimageview)
 
-    implementation ("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
-    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+    implementation (libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation (libs.okhttp)
 
     implementation ("com.kakao.sdk:v2-user:2.20.0")
 
